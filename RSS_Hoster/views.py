7eb4f33d84b8fbbac3f5a308ed5f237f78ace469
CarlosArch <carlos_arch@hotmail.com>
2021-07-16 14:46:54 -0500

    Allow to limit number of tweets

--- RSS_Hoster/views.py
+++ RSS_Hoster/views.py
@@ -10,12 +10,14 @@ def feed(request):
     screen_name = request.GET.get('screen_name')
     no_replies = request.GET.get('no_replies')
     media_only = request.GET.get('media_only')
+    limit = request.GET.get('limit')
 
     _, api = connect_to_twitter('.twitter.json')
     timeline = get_user_timeline(api=api
                                 ,screen_name=screen_name
                                 ,no_replies=no_replies
-                                ,media_only=media_only)
+                                ,media_only=media_only
+                                ,limit=limit)
 
     RSS = format_to_rss(timeline, screen_name)
     return HttpResponse(RSS, content_type='text/xml')
